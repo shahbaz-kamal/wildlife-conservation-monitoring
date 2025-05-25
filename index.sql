3eb0c692db66788b32f3eb29aae009badec8d26b
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-25 15:10:26 +0600

    two table created

--- index.sql
+++ index.sql
@@ -1,3 +1,24 @@
 -- Active: 1747504319902@@localhost@5432@conservation_db@public
 CREATE DATABASE conservation_db
 --// creating tables
+CREATE TABLE
+    rangers (
+        ranger_id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        region TEXT NOT NULL,
+    )
+CREATE TABLE
+    species (
+        species_id SERIAL PRIMARY KEY,
+        common_name VARCHAR(50) NOT NULL,
+        conservation_status TEXT NOT NULL CHECK (
+            conservation_status IN (
+                'Critically Endangered',
+                'Endangered',
+                'Vulnerable',
+                'Least Concern'
+            )
+        )
+    )
+
+    
